384cb518c9aec4594a07ae562422fa9e5a74ec27
Franco Aguilera <flaalf@gmail.com>
2025-03-13 13:54:51 -0300

    personal CV website

---
+++ src/utils/index.ts
@@ -0,0 +1,3 @@
+import CV from "@cv/resume.json" assert { type: "json" };
+
+export const getJsonCV = () => CV;
---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	darkMode: ["class", '[data-skin-mode="dark"]'],
+	theme: {
+		extend: {
+			animation: {
+				"appear-bottom": "appear-bottom 1ms linear both",
+			},
+			keyframes: {
+				"appear-bottom": {
+					from: { opacity: 0, bottom: "-200px" },
+					to: { opacity: 1, bottom: 0 },
+				},
+			},
+		},
+	},
+	plugins: [],
+};
